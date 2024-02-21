f6c189bccaabb5e31ae0664da0f3e52e5f6fd600
eaberkov <eaberkov@ibs.ru>
2024-02-21 21:47:18 +0300

    Added win-processes killing

---
+++ FC12/SupportingTasks/WindowsProcessHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FC12.SupportingTasks
+{
+    public static class WindowsProcessHandler
+    {
+        public static readonly string[] _processNamesToKill = 
+        {
+            "ContentCapture", 
+            "FlexiBR", 
+            "StationMonitor"
+        };
+
+        public static void KillWindowsProcess()
+        {
+
+            foreach (Process process in GetProcesses())
+            {
+                //MessageBox.Show(process.ProcessName);
+                process.Kill();
+            }
+        }
+
+        private static List<Process> GetProcesses()
+        {
+            List<Process> result = new List<Process>();
+
+            foreach (string processName in _processNamesToKill)
+            {
+                var existingProcesses = Process.GetProcessesByName(processName).ToList();
+
+                //MessageBox.Show(processName + " " + existingProcesses.Count);
+
+                if (existingProcesses != null)
+                {
+                    result = result.Concat(existingProcesses).ToList();
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/ProcessKillingTests.cs
@@ -0,0 +1,22 @@
+﻿using FC12.SupportExtensions.Outlook;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FC12.SupportingTasks;
+
+
+namespace Tests
+{
+        [TestClass]
+        public class ProcessKillingTests
+        {
+            [TestMethod]
+            public void TestKillProcesses()
+            {
+                WindowsProcessHandler.KillWindowsProcess();
+            }
+        }
+}
