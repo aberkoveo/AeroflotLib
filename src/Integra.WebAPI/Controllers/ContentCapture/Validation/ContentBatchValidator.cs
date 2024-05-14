634ac931efb10c44cc7ea1e53616cb023354b1b5
eaberkov <eaberkov@ibs.ru>
2024-05-14 19:05:44 +0300

    add fluent validation

---
+++ src/Integra.WebAPI/Controllers/ContentCapture/Validation/ContentBatchValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Integra.Domain.ContentCapture;
+
+namespace Integra.WebAPI.Controllers.ContentCapture.Validation
+{
+    public class ContentBatchValidator : AbstractValidator<ContentBatch>
+    {
+        public ContentBatchValidator()
+        {
+            RuleFor(batch => batch.Name).NotEmpty();
+            RuleFor(batch => batch.ProjectId).NotEmpty();
+            RuleFor(batch => batch.BatchTypeId).NotEmpty();
+            RuleFor(batch => batch.Name).NotEmpty();
+            RuleFor(batch => batch.OwnerId).NotEmpty();
+            RuleFor(batch => batch.RegistrationParameters).NotNull();
+
+            RuleFor(batch => batch.RegistrationParameters)
+                .Must(parameters => parameters.ContainsKey("Станция сканирования"))
+                .WithMessage("Не заполнен параметр \"Станция сканирования\"");
+
+            RuleFor(batch => batch.RegistrationParameters)
+                .Must(parameters => parameters.ContainsKey("Сканировщик"))
+                .WithMessage("Не заполнен параметр \"Сканировщик\"");
+
+
+        }
+    }
+}
